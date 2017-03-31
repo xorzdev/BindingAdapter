185f8aa8ecb591fb94ad0cf9421ace3b7003e9d5
gavinxxxxxx <gavinxxxxxx@gmail.com>
2017-03-31 15:07:16 +0800

    。

--- app/src/main/java/com/gavin/magicadapter/MainActivity.java
+++ app/src/main/java/com/gavin/magicadapter/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private String[] images = new String[] {
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490951276314&di=be8ef05f74ab576234f00664ab97556a&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fpic%2F0%2F50%2F5ffd1286284.jpg",
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490951291472&di=2c818cffb380050a5044cd8602cfb3cf&imgtype=0&src=http%3A%2F%2Fimg.popo.cn%2Fuploadfile%2F2017%2F0217%2F1487316191770266.jpg",
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490951300870&di=4963b101ddffdda57c659b9dc95f52e5&imgtype=0&src=http%3A%2F%2Fimage.3761.com%2Fnvxing%2F2016022923%2F2016022923025165610.jpg",
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490951311588&di=0e3adeec2b0438f85fbd208e9802adc9&imgtype=0&src=http%3A%2F%2Fnpic7.edushi.com%2Fcn%2Fzixun%2Fzh-chs%2F2017-03%2F03%2F3822397-2017030223252852.jpg",
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490951329510&di=1d5699eb36c5c5bd9848289e299cbfaf&imgtype=0&src=http%3A%2F%2Fd.5857.com%2Fqqny_170228%2Fdesk_009.jpg",
+            "http://img04.sogoucdn.com/app/a/100520024/aaaa5ece8c214c800d8932029dd533d4",
+            "http://img02.sogoucdn.com/app/a/100520024/4f0c1a1eb2423dd56acd655ea5b291ce",
+            "http://img02.sogoucdn.com/app/a/100520024/48f346864ba87289fe755986f5fc05e3",
+            "http://img04.sogoucdn.com/app/a/100520024/acfeb1fdb3ef946561db8d8ea77b4152",
+            "http://img02.sogoucdn.com/app/a/100520024/77d27afcb273278189591baf724d3bcf",
     };
 
     @Override
